6c3d8490a1f95df9449c0c361ad3ddfb2382bb28
Nic Burkinshaw <nicbkw@outlook.com>
2019-10-18 16:47:03 +0100

    Update viewer.js
    removed insertDocument from external reference

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const {insertDocument, getArray} = require('./mongo');
+const {getArray} = require('./mongo');
 
 app.set('view engine', 'hbs');
 
